3aecb2ba8514988c063c1d381df31049c7fbca64
Neal Terrell <neal.terrell@csulb.edu>
2016-12-26 16:12:36 -0800

    NUnit install.

--- Introduction/Class1.cs
+++ Introduction/Class1.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Introduction
-{
-    public class Class1
-    {
-        public Class1()
-        {
-        }
-    }
+namespace Introduction {
+	public class Class1 {
+		public Class1() {
+
+		}
+
+		public static int TestSomething() {
+			return 1;
+		}
+	}
 }
