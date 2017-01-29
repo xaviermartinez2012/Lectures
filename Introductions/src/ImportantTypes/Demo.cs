50aa25df867181efeea7e06f054c2d46011c15b2
Neal Terrell <neal.terrell@csulb.edu>
2017-01-29 09:45:09 -0800

    Lessons on CLI types.

---
+++ Introductions/src/ImportantTypes/Demo.cs
@@ -0,0 +1,62 @@
+﻿namespace ImportantTypes {
+	// A recreation of important types that C# programmers should know.
+
+	interface IComparable {
+		int CompareTo(object other);
+	}
+	interface IComparable<T> {
+		int CompareTo(T other);
+	}
+	// What's the difference?
+
+	interface IEquatable<T> {
+		bool Equals(T other);
+	}
+	// How is that different than Object.Equals?
+
+	// IEnumerable is a read-only sequence of values.
+	interface IEnumerable<T> {
+		IEnumerator<T> GetEnumerator();
+	}
+
+	// IEnumerator gives access to individual elements of an IEnumerable.
+	interface IEnumerator<T> {
+		T Current { get; }
+		bool MoveNext();
+	}
+
+	// ICollection is a container that can add and remove elements. Collection<T> in Java.
+	interface ICollection<T> : IEnumerable<T> { 
+		void Add(T item);
+		void Remove(T item);
+		bool Contains(T item);
+		int Count { get; }
+	}
+
+	// IList is a list container (items indexed by integer position). List<T> in Java.
+	interface IList<T> : ICollection<T> { // thus, also IEnumerable<T>
+		T this[int index] { get; set; } // syntax for overloading operator []
+		int IndexOf(T item);
+		void Insert(int index, T item);
+		void RemoveAt(int index);
+	}
+
+	// List<T> implements IList<T> using an array list. ArrayList<T> in Java.
+	
+
+	// A key/value pair for use in maps. (Map.Entry in Java.)
+	struct KeyValuePair<TKey, TValue> {
+		public TKey Key { get; }
+		public TKey Value { get; }
+	}
+
+	// IDictionary is a map (dictionary) structure from keys to values. Map<TKey, TValue> in Java.
+	interface IDictionary<TKey, TValue> : ICollection<KeyValuePair<TKey, TValue>> {
+		void Add(TKey key, TValue value);
+		bool ContainsKey(TKey key);
+		void Remove(TKey key);
+	}
+
+	// Dictionary<TKey, TValue> implements IDictionary<TKey, TValue> using a hashtable with open addressing.
+	// HashMap<TKey, TValue> in Java.
+}
---
+++ Introductions/src/TypeDemos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TypeDemos {
+	public class Program {
+		public static void PrintObject(object obj) {
+			// The C# keyword "object" is an alias for the CLI type Object.
+			Console.WriteLine("{0} has HashCode {1}", obj.ToString(), obj.GetHashCode());
+		}
+
+		public static void BoxInt(object obj) {
+			int value = (int)obj;
+			Console.WriteLine("Boxed object: {0}; unboxed int: {1}", obj, value);
+		}
+
+		public static void Main(string[] args) {
+			// PrintObject's parameter is compatible with any argument type.
+			PrintObject(5); // a ValueType of Int32
+			PrintObject("Hello"); // a ReferenceType of String
+
+			// demonstrate boxing an int
+			BoxInt(10);
+
+			// which of these array assignments are boxed?
+			object[] arr = new object[5];
+			arr[0] = "Hello";
+			arr[1] = 10;
+			arr[2] = 'a';
+			arr[3] = true;
+			arr[4] = new int[1];
+		}
+	}
+}
