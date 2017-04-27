e6c7e2c2e6116cec9c9bef0861b51b13c3edd912
Neal Terrell <neal.terrell@csulb.edu>
2017-04-26 20:50:49 -0700

    Web API + Entity Framework + WPF example.

---
+++ WebAPI/Calculator/src/Cecs475.Web.Calculator/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace Cecs475.Web.Calculator {
+	public static class WebApiConfig {
+		public static void Register(HttpConfiguration config) {
+			// Web API configuration and services
+
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+
+			// We remove the auto-generated "MapHttpRoute" method calls which set up convention-based routing.
+
+			GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+				.Add(new RequestHeaderMapping("Accept",
+						"text/html",
+						StringComparison.InvariantCultureIgnoreCase,
+						true,
+						"application/json"));
+		}
+	}
+}
